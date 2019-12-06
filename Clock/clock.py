c5661cb5ba529964d9f4f44d56b443b2be5a0cc3
ethan <Enprogames@users.noreply.github.com>
2019-12-06 14:55:59 -0800

    asdfasdfasdfasdfasdf

--- Clock/clock.py
+++ Clock/clock.py
@@ -59,7 +59,7 @@ def get_joke():
         headers = {'User-Agent': 'My Library (https://github.com/Enprogames/Python-Clock/)', 'Accept': 'application/json'}
         return requests.get('https://icanhazdadjoke.com/', headers=headers).json().get('joke')
     except:
-        f = open('tmp/dadjoke', 'r')
+        f = open('home/tmp/dadjoke', 'r')
         return f.read()
 
 def set_joke():
